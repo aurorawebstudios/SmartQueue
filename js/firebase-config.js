d2e2dc61caad89d07ab01f8e398796459317aaa3
PeakSiteStudio <aurorawebestudio@gmail.com>
2026-05-31 22:22:02 +0200

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,22 @@
+// js/firebase-config.js
+const firebaseConfig = {
+    apiKey: "TU_API_KEY",
+    authDomain: "smartqueue-2026.firebaseapp.com",
+    projectId: "smartqueue-2026",
+    storageBucket: "smartqueue-2026.firebasestorage.app",
+    messagingSenderId: "XXXXXXXXXXXX",
+    appId: "1:XXXXXXXXXXXX:web:XXXXXXXXXXXXXXXXXX"
+};
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Services
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+// Exportar para usar en otros archivos
+window.auth = auth;
+window.db = db;
+window.storage = storage;
